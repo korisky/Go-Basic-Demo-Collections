1930687b1162b01c25408fb2652a29112039eebd
roylic <lykismekk@outlook.com>
2023-08-05 13:40:58 +0800

    adding generic claim unit test demo

---
+++ iden3/genericClaim_test.go
@@ -0,0 +1,38 @@
+package idenDemo
+
+import (
+	"encoding/json"
+	"fmt"
+	core "github.com/iden3/go-iden3-core"
+	"math/big"
+	"testing"
+	"time"
+)
+
+func Test_GenericClaim(t *testing.T) {
+
+	// set claim expiration date
+	time := time.Date(1361, 3, 22, 0, 44, 48, 0, time.UTC)
+
+	// set schema
+	ageSchema, _ := core.NewSchemaHashFromHex("2e2d1c11ad3e500de68d7ce16a0a559e")
+
+	// define data slots
+	birthday := big.NewInt(199960424)
+	documentType := big.NewInt(1)
+
+	// set revocation nonce
+	revocationNonce := uint64(1909830690)
+
+	// set ID of the claim subject
+	id, _ := core.IDFromString("113TCVw5KMeMp99Qdvub9Mssfz7krL9jWNvbdB7Fd2")
+
+	// create claim
+	claim, _ := core.NewClaim(ageSchema, core.WithExpirationDate(time),
+		core.WithRevocationNonce(revocationNonce), core.WithIndexID(id),
+		core.WithIndexDataInts(birthday, documentType))
+
+	// transform claim from bytes array to json
+	claimToMarshal, _ := json.Marshal(claim)
+	fmt.Println(string(claimToMarshal))
+}
--- iden3/go.mod
+++ iden3/go.mod
@@ -6,7 +6,9 @@ require github.com/iden3/go-iden3-crypto v0.0.15
 
 require (
 	github.com/dchest/blake512 v1.0.0 // indirect
+	github.com/iden3/go-iden3-core v1.0.2 // indirect
 	github.com/iden3/go-merkletree-sql v1.0.2 // indirect
+	github.com/mr-tron/base58 v1.2.0 // indirect
 	golang.org/x/crypto v0.7.0 // indirect
 	golang.org/x/sys v0.6.0 // indirect
 )
