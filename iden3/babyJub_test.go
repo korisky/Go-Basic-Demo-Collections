4f5170abafc9d566e77873ebd1471374e2410c34
roylic <lykismekk@outlook.com>
2023-08-05 12:57:19 +0800

    adding simple baby jubjub algo calling test

---
+++ iden3/babyJub_test.go
@@ -0,0 +1,20 @@
+package babyjub
+
+import (
+	"fmt"
+	"github.com/iden3/go-iden3-crypto/babyjub"
+	"testing"
+)
+
+// Baby Jubjub is the elliptic curve that used in Iden3, which is designed to work efficiently with zkSNARKs
+// https://docs.iden3.io/publications/pdfs/Baby-Jubjub.pdf
+func Test_BabyJubGen(t *testing.T) {
+
+	// generate private key randomly
+	babyJubjubPrivKey := babyjub.NewRandPrivKey()
+
+	// generate pub key from priKey
+	babyJubjubPubKey := babyJubjubPrivKey.Public()
+
+	fmt.Println(babyJubjubPubKey.String())
+}
