a9831fbcc94d05ae232fabda46633e32c973d989
roylic <lykismekk@outlook.com>
2023-08-12 13:48:23 +0800

    adding closure example

---
+++ gofeatures/closure/closure_test.go
@@ -0,0 +1,31 @@
+package closure
+
+import (
+	"fmt"
+	"testing"
+)
+
+// Test_Closure is an example for closure feature in golang
+// compare to Java's anonymous inner class, which can only access final variables
+// golang's closure can be used with much more flexibility
+func Test_Closure(t *testing.T) {
+	x := 10
+	increment := func() int {
+		x++
+		return x
+	}
+	fmt.Println(increment())
+	fmt.Println(increment())
+}
+
+// Test_ClosureCallback usually we use closure in golang as we manipulate Callback
+func Test_ClosureCallback(t *testing.T) {
+	f := adder(10)
+	fmt.Println(f(5))
+}
+
+func adder(x int) func(int) int {
+	return func(y int) int {
+		return x + y
+	}
+}
