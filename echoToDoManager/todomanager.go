d01af2090d69715fb6e8d32fe582f6b80e6bc81d
roylic <lykismekk@outlook.com>
2023-08-24 22:39:28 +0800

    adding simple todo manager using echo framework

---
+++ echoToDoManager/todomanager.go
@@ -0,0 +1,22 @@
+package echoToDoManager
+
+import "sync"
+
+// Todo represents the todo bullet points
+type Todo struct {
+	ID         string `json:"id"`
+	Title      string `json:"title"`
+	IsComplete bool   `json:"is_complete"`
+}
+
+type TodoManager struct {
+	todos []Todo
+	m     sync.Mutex // operation to be atomic
+}
+
+func NewTodoManager() TodoManager {
+	return TodoManager{
+		todos: make([]Todo, 0),
+		m:     sync.Mutex{},
+	}
+}
