924377e3ffc619f49101ec7d6b7b68395230b2e0
roylic <lykismekk@outlook.com>
2023-08-28 22:59:58 +0800

    add db migrate & some kind of aop stuff

--- webProjects/gormDemo/gormDemo.go
+++ webProjects/gormDemo/gormDemo.go
@@ -1,17 +1,45 @@
 package main
 
 import (
+	"errors"
+	"fmt"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"log"
+	"strings"
 )
+
+// User define simple struct -> we can use gorm.Model for convenience
+type User struct {
+	gorm.Model // similar to extend
+
+	Name    string
+	Address string
+}
+
+// BeforeCreate can work as 'aop' that it would work before the user be created
+func (u *User) BeforeCreate(tx *gorm.DB) (err error) {
+	fmt.Println("Before creation")
+	return
+}
+
+func (u *User) BeforeUpdate(tx *gorm.DB) (err error) {
+	if strings.Contains(u.Name, "Tom") {
+		err = errors.New("Tom could not be updated")
+	}
+	return
+}
 
 func main() {
 
 	// mysql set up for gorm
-	dsn := "root:12345678@tcp(127.0.0.1:3306)/epay_payment?charset=utf8mb4&parseTime=True&loc=Local"
-	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	dsn := "root:12345678@tcp(127.0.0.1:3306)/go_use?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatalln(err)
 	}
+
+	// auto migrate (create tables & missing stuff, would not delete unused columns)
+	_ = db.AutoMigrate(&User{})
+
 }
