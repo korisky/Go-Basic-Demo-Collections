e41089397330e0b3e1bc6de8658459754ad3dfe1
roylic <lykismekk@outlook.com>
2023-09-06 23:19:28 +0800

    successfully set-up a HTTP3 server in Golang with QUIC protocol

--- http3/quic-go/server.go
+++ http3/quic-go/server.go
@@ -1,56 +1,20 @@
 package main
 
 import (
-	"context"
-	"crypto/rand"
-	"crypto/rsa"
-	"crypto/tls"
-	"crypto/x509"
-	"encoding/pem"
-	"fmt"
-	"github.com/quic-go/quic-go"
-	"io"
-	"log"
-	"math/big"
+	"github.com/quic-go/quic-go/http3"
 	"net/http"
 )
 
 func main() {
 
-}
+	keyFile := "/Users/roylic/PEM/key.unencrypted.pem"
+	certFile := "/Users/roylic/PEM/cacert.pem"
 
-type loggingWriter struct {
-	io.Writer
-}
-
-func (w loggingWriter) Write(b []byte) (int, error) {
-	fmt.Println("Server: Got: '%s'\n", string(b))
-	return w.Writer.Write(b)
-}
-
-// generateTLSConfig -> generate QUIC needed tls config
-func generateTLSConfig() *tls.Config {
-	key, err := rsa.GenerateKey(rand.Reader, 1024)
+	http.HandleFunc("/hello", func(writer http.ResponseWriter, request *http.Request) {
+		writer.Write([]byte("Hello, Http3"))
+	})
+	err := http3.ListenAndServeQUIC("localhost:4242", certFile, keyFile, nil)
 	if err != nil {
-		log.Fatalln(err)
-	}
-
-	template := x509.Certificate{SerialNumber: big.NewInt(1)}
-	certDER, err := x509.CreateCertificate(rand.Reader, &template, &template, &key.PublicKey, key)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	keyPEM := pem.EncodeToMemory(&pem.Block{Type: "RAS PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(key)})
-	certPEM := pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE", Bytes: certDER})
-
-	tlsCert, err := tls.X509KeyPair(certPEM, keyPEM)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	return &tls.Config{
-		Certificates: []tls.Certificate{tlsCert},
-		NextProtos:   []string{"go-quic-demo"},
+		panic(err)
 	}
 }
