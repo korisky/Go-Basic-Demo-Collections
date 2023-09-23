85789804a99f11d60951df54fec01c1e37d5be15
roylic <lykismekk@outlook.com>
2023-09-23 13:47:26 +0800

    map, with the feature of not enabling addressing, could only change the value inside it by resetting. But for slice, it works on directly changing

--- gofeatures/mistakes/hard_mistakes_test.go
+++ gofeatures/mistakes/hard_mistakes_test.go
@@ -26,7 +26,25 @@ func Test_pointer_negative(t *testing.T) {
 	}
 	fmt.Println(m["x"].name)
 
-	//m["x"].print() // could not call it by this under map
+	//m["x"].print() // could not call it by this under map, because map can not be addressing
 	theX := m["x"]
 	theX.print() // but after assign it to a variable, can call the function again
+}
+
+func Test_value_changing(t *testing.T) {
+
+	// map can not be addressing -> could not change the value directly
+	m := map[string]data{
+		"x": {"TOM"},
+	}
+	//m["x"].name = "Jerry" // could not be addressing
+	d := m["x"]
+	d.name = "April"
+	m["x"] = d
+	fmt.Println(m) // this is the way for changing the value inside map
+
+	// but for slice, it works
+	s := []data{{"Tom"}}
+	s[0].name = "Jerry"
+	fmt.Println(s)
 }
