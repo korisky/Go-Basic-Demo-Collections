4f198f668a7bda0bcd16d0c491626f3e4b10f2f4
roylic <lykismekk@outlook.com>
2023-09-23 11:37:11 +0800

    two simple way to solve that in golang it would pass copy of the array, rather than passing an reference (not reference -> changes not remain)

--- gofeatures/mistakes/simple_mistakes_test.go
+++ gofeatures/mistakes/simple_mistakes_test.go
@@ -18,3 +18,35 @@ func Test_closure(t *testing.T) {
 	}
 	fmt.Printf("X:%v, Y:%v\n", x, y)
 }
+
+func Test_closure_v2(t *testing.T) {
+	x := [3]int{1, 2, 3}
+	func(arr [3]int) {
+		// In golang, array also wold send 'value copy', unlike Java & C++, they send array as 'reference'
+		// thus, in golang, array's value manipulation would not remain after stack exit
+		arr[0] = 7
+		fmt.Println(arr)
+	}(x)
+	fmt.Println(x)
+	fmt.Println()
+
+	// two saving ways ->
+	// 1) pass pointer, and manipulate the value
+	y := [3]int{1, 2, 3}
+	func(arr *[3]int) {
+		arr[0] = 7
+		fmt.Println(*arr)
+	}(&y)
+	fmt.Println(y)
+	fmt.Println()
+
+	// 2) use slice rather than array
+	z := y[:]
+	func(arr []int) {
+		arr[0] = 9
+		fmt.Println(arr)
+	}(z)
+	fmt.Println(y)
+	fmt.Println(z)
+	fmt.Println()
+}
