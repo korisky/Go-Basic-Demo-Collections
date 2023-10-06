69b8811de51c30c327e22289ea017e347ee316f4
roylic <lykismekk@outlook.com>
2023-10-06 14:04:55 +0800

    simple using fmt.Errorf rather pure panic()

--- http3/quic-go/server.go
+++ http3/quic-go/server.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"github.com/quic-go/quic-go/http3"
 	"net/http"
+	"time"
 )
 
 func main() {
@@ -15,6 +17,7 @@ func main() {
 	})
 	err := http3.ListenAndServeQUIC("localhost:4242", certFile, keyFile, nil)
 	if err != nil {
-		panic(err)
+		err = fmt.Errorf("error occurred at: %v, with details: %v", time.Now(), err.Error())
+		fmt.Println(err.Error())
 	}
 }
