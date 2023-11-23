86ceb365de9bf6169da348513ec719b300e4728e
roylic <35546172+EricLuoisme@users.noreply.github.com>
2023-11-23 14:13:17 +0800

    init the Gin framework basic endpoint

---
+++ webProjects/ginDemo/rate/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+
+	router := gin.Default()
+
+	router.GET("/api/functionx/info", func(context *gin.Context) {
+		context.JSON(200, gin.H{
+			"message": "info",
+		})
+	})
+
+	err := router.Run()
+	if err != nil {
+		return
+	}
+}
