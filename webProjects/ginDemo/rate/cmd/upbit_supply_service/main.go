061a48d9fe6049d6e311da2576a6768913f2e00b
roylic <35546172+EricLuoisme@users.noreply.github.com>
2023-11-24 16:48:20 +0800

    feat: implement the server start logic by embedding cache and scheduling task

--- webProjects/ginDemo/rate/cmd/upbit_supply_service/main.go
+++ webProjects/ginDemo/rate/cmd/upbit_supply_service/main.go
@@ -3,25 +3,73 @@ package main
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/patrickmn/go-cache"
+	"github.com/robfig/cron/v3"
 	"log"
 	"own/gin/rate/internal/handler"
 	"own/gin/rate/internal/load"
 	"strconv"
+	"time"
 )
 
+// ApiPath is the expose path, e.g. /api/fx/info, /api/pundix/info
 const ApiPath = "/api/%s/info"
 
 func main() {
-	// load configuration
+	// 1. load configuration
+	config, err, apiPath, failed := loadConfigProcess()
+	if failed {
+		return
+	}
+
+	// 2. init cache & run next
+	c, cr := initCacheProcess(config)
+
+	// 3. init scheduler
+	err, failed = scheduleSetting(err, cr, c, config)
+	if failed {
+		return
+	}
+
+	// 4. start web service
+	startWebServiceProcess(apiPath, config, err)
+}
+
+// loadConfigProcess for loading the configs from config.json
+func loadConfigProcess() (*load.Config, error, string, bool) {
 	config, err := load.LoadConfiguration()
 	if err != nil {
 		log.Fatal(err)
-		return
-
+		return nil, nil, "", true
 	}
 	apiPath := fmt.Sprintf(ApiPath, config.NodeServing)
+	return config, err, apiPath, false
+}
 
-	// start service
+// initCacheProcess for init the cache & scheduler
+func initCacheProcess(config *load.Config) (*cache.Cache, *cron.Cron) {
+	c := cache.New(65*time.Minute, 75*time.Minute)
+	cr := cron.New()
+	// run the fetching task, async
+	go handler.SchedulingTask(c, config)
+	return c, cr
+}
+
+// scheduleSetting for init the scheduler & task
+func scheduleSetting(err error, cr *cron.Cron, c *cache.Cache, config *load.Config) (error, bool) {
+	_, err = cr.AddFunc("@every 1h", func() {
+		handler.SchedulingTask(c, config)
+	})
+	if err != nil {
+		log.Fatal(err)
+		return nil, true
+	}
+	cr.Start()
+	return err, false
+}
+
+// startWebServiceProcess expose the API and serve
+func startWebServiceProcess(apiPath string, config *load.Config, err error) {
 	router := gin.Default()
 	router.GET(apiPath,
 		func(context *gin.Context) {
---
+++ webProjects/ginDemo/rate/internal/handler/schedule_handler.go
@@ -0,0 +1,10 @@
+package handler
+
+import (
+	"github.com/patrickmn/go-cache"
+	"own/gin/rate/internal/load"
+)
+
+func SchedulingTask(cache *cache.Cache, config *load.Config) {
+
+}
