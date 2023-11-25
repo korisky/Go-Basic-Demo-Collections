88fe7d562829a9b9648b15be41b2b97a9647371a
roylic <35546172+EricLuoisme@users.noreply.github.com>
2023-11-25 22:53:41 +0800

    refactor: concise & upgrade the fetching inner logic

--- webProjects/ginDemo/rate/pkg/supply/supply_fetch.go
+++ webProjects/ginDemo/rate/pkg/supply/supply_fetch.go
@@ -2,11 +2,58 @@ package supply
 
 import (
 	"encoding/json"
+	"errors"
+	"log"
+	"math"
 	"net/http"
+	"own/gin/rate/internal/load"
+	"strconv"
+	"strings"
 )
 
-// FetchSupply will retrieve denom (fx / pundix) supply from the given node url
-func FetchSupply(nodeUrl string) (*SupplyApiResponse, error) {
+// FetchCirculatingSupply will retrieve Denom circulating supply
+func FetchCirculatingSupply(config *load.Config) (float64, error) {
+	// fetch supply
+	supplyResp, err := fetchSupply(config.NodeUrl)
+	if err != nil {
+		log.Fatal(err)
+		return 0, err
+	}
+	// extract supply
+	supply, err := extractTargetDenomCirculatingSupply(supplyResp, config)
+	if err != nil {
+		return 0, err
+	}
+	// decimals
+	return supply / math.Pow10(18), nil
+}
+
+// extractTargetDenomCirculatingSupply help extract target denom supply
+func extractTargetDenomCirculatingSupply(supplyResp *SupplyApiResponse, config *load.Config) (float64, error) {
+	for _, item := range supplyResp.Supply {
+		if isRelevantDenom(item, config) {
+			return strconv.ParseFloat(item.Amount, 64)
+		}
+	}
+	return 0, errors.New("relevant denom not found")
+}
+
+// isRelevantDenom filter for target denom
+func isRelevantDenom(item SupplyItem, config *load.Config) bool {
+	switch {
+	case
+		load.FxServing == config.NodeServing && strings.EqualFold("fx", item.Denom):
+		return true
+	case
+		load.PundixServing == config.NodeServing && strings.EqualFold("ibc/55367B7B6572631B78A93C66EF9FDFCE87CDE372CC4ED7848DA78C1EB1DCDD78", item.Denom):
+		return true
+	default:
+		return false
+	}
+}
+
+// fetchSupply will retrieve denom (fx / pundix) circulating supply from the given node url
+func fetchSupply(nodeUrl string) (*SupplyApiResponse, error) {
 	// request
 	resp, err := http.Get(nodeUrl + SUPPLY_PATH)
 	if err != nil {
