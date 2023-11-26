6ad9d21a1bff235ac5602a4338c1d398497eec3d
roylic <35546172+EricLuoisme@users.noreply.github.com>
2023-11-26 23:22:30 +0800

    test: add unit tests for both new interfaces on fetching quote prices

--- webProjects/ginDemo/rate/pkg/exchange/openexchange/price_openexchange_test.go
+++ webProjects/ginDemo/rate/pkg/exchange/openexchange/price_openexchange_test.go
@@ -17,3 +17,15 @@ func Test_FetchOpenExchange(t *testing.T) {
 	jsonStr, _ := json.MarshalIndent(price, "", "  ")
 	fmt.Println(string(jsonStr))
 }
+
+func Test_FetchQuotePrice(t *testing.T) {
+	apiKey := ""
+	fetch := OxFetcher{UsdPrice: float64(1), ApiKey: apiKey}
+	prices, err := fetch.FetchConvertToQuotePrices()
+	if err != nil {
+		log.Fatalln(err)
+		return
+	}
+	jsonStr, _ := json.MarshalIndent(*prices, "", "  ")
+	fmt.Println(string(jsonStr))
+}
