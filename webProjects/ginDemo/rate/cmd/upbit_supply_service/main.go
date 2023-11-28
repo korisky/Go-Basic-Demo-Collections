f73125a7c449409746d791705f46fe11643516fc
roylic <35546172+EricLuoisme@users.noreply.github.com>
2023-11-28 22:37:39 +0800

    fix: correct the coding logic for returning error rather then bool val

--- webProjects/ginDemo/rate/cmd/upbit_supply_service/main.go
+++ webProjects/ginDemo/rate/cmd/upbit_supply_service/main.go
@@ -16,32 +16,40 @@ import (
 // ApiPath is the expose path, e.g. /api/fx/info, /api/pundix/info
 const ApiPath = "/api/%s/info"
 
+// ConfigPath is for default config location
+const ConfigPath = "config/config.json"
+
 func main() {
 	// 1. load configuration
-	config, apiPath, failed := loadConfigProcess()
-	if failed {
+	//configPath := ConfigPath
+	//if len(os.Args) >= 2 {
+	//	configPath = os.Args[1]
+	//}
+
+	config, err := loadConfigProcess()
+	if err != nil {
 		return
 	}
 	// 2. init cache & run next
 	c, cr := initCacheProcess(config)
 	// 3. init scheduler
-	failed = scheduleSetting(c, cr, config)
-	if failed {
+	err = scheduleSetting(c, cr, config)
+	if err != nil {
 		return
 	}
 	// 4. start web service
+	apiPath := fmt.Sprintf(ApiPath, config.NodeServing)
 	startWebServiceProcess(c, apiPath, config)
 }
 
 // loadConfigProcess for loading the configs from config.json
-func loadConfigProcess() (*load.Config, string, bool) {
+func loadConfigProcess() (*load.Config, error) {
 	config, err := load.LoadConfiguration("config/config.json")
 	if err != nil {
 		log.Fatal(err)
-		return nil, "", true
+		return nil, err
 	}
-	apiPath := fmt.Sprintf(ApiPath, config.NodeServing)
-	return config, apiPath, false
+	return config, nil
 }
 
 // initCacheProcess for init the cache & scheduler
@@ -54,16 +62,16 @@ func initCacheProcess(config *load.Config) (*cache.Cache, *cron.Cron) {
 }
 
 // scheduleSetting for init the scheduler & task
-func scheduleSetting(c *cache.Cache, cr *cron.Cron, config *load.Config) bool {
+func scheduleSetting(c *cache.Cache, cr *cron.Cron, config *load.Config) error {
 	_, err := cr.AddFunc("@every 1h", func() {
 		schedule.SchedulingFeedPriceToCache(c, config)
 	})
 	if err != nil {
 		log.Fatal(err)
-		return true
+		return err
 	}
 	cr.Start()
-	return false
+	return nil
 }
 
 // startWebServiceProcess expose the API and serve
