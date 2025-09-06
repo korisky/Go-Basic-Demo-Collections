916519703e682a036173d6bc1b595f28bbfc32bc
Roylic <35546172+korisky@users.noreply.github.com>
2025-09-06 13:07:40 +0800

    feat: simple test for both buf & non-buf channel's behavior

--- goroutines/bufChan_test.go
+++ goroutines/bufChan_test.go
@@ -1,34 +1,38 @@
 package goroutines
 
 import (
+	"fmt"
 	"testing"
 	"time"
 )
 
-func TestBufChan(t *testing.T) {
-
+// TestNoBufChan 没有使用buf, 可以得到结果发现finish基本都慢于consume (因为chan只有1的capacity, 另一个投递就block住)
+func TestNoBufChan(t *testing.T) {
 	// 无buffer的channel, 接收一个值之后, 立刻被block
 	ch := make(chan int)
-
-	// 测试无buffer channel的行为
-	noBufChanPlay(ch)
-
-	// 这个与 make(chan int) 不同点在于, 它是有buffer的channel
-	// 只有同时hold住2个值的时候, 投递者才会被block
-	chBuf := make(chan int, 2)
-	_ = chBuf // 暂时标记为已使用，避免编译错误
+	chanPlay(ch)
 }
 
-// noBufChanPlay -> 多次尝试可以发现, 这里取出消费先后次序是不能确保的
-func noBufChanPlay(ch chan int) {
+// TestBufChan 使用buf, 可以得到结果发现finish基本都早于consume（因为chan有buffer, 投递都没有block）
+func TestBufChan(t *testing.T) {
+	// 只有同时hold住2个值的时候, 投递者才会被block
+	chBuf := make(chan int, 2)
+	chanPlay(chBuf)
+}
+
+// chanPlay -> 多次尝试可以发现, 这里取出消费先后次序是不能确保的
+func chanPlay(ch chan int) {
+
 	// 异步投递一个
 	go func(ch chan int) {
 		ch <- 1
+		fmt.Println("chan input 1 finish")
 	}(ch)
 
 	// 异步投递一个
 	go func(ch chan int) {
 		ch <- 3
+		fmt.Println("chan input 3 finish")
 	}(ch)
 
 	// 等待1s
