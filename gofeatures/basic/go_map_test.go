429fc607d69f6490e1f25a767cc55f21160e4bf1
Roylic <35546172+korisky@users.noreply.github.com>
2025-10-17 13:03:38 +0800

    feat: simple map contain or not with assertion's help

--- gofeatures/basic/go_map_test.go
+++ gofeatures/basic/go_map_test.go
@@ -24,3 +24,20 @@ var m = map[string]TheVertex{
 func TestMapInit(t *testing.T) {
 	fmt.Println(m)
 }
+
+// TestMapContaining 可以看出，类似interface中我们可以使用断言确认类型并获取值
+// map中同样可以, 简单直接同时获取值+是否存在
+func TestMapContaining(t *testing.T) {
+	m := make(map[string]string)
+	m["a"] = "123"
+	m["b"] = "456"
+
+	sa, ok := m["a"]
+	fmt.Println(sa, ok)
+
+	sc, ok := m["c"]
+	fmt.Println(sc, ok)
+
+	scc := m["c"]
+	fmt.Println(scc)
+}
