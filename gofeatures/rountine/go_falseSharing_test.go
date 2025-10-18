7384f1d4b2f60d1cb737eadca3de02134dc263fc
Roylic <35546172+korisky@users.noreply.github.com>
2025-10-18 13:01:55 +0800

    pref: impl the fully benchmark identifier with false-sharing, and with padding to prevent them, almost 200% greater than the false-sharing one

--- gofeatures/rountine/go_falseSharing_test.go
+++ gofeatures/rountine/go_falseSharing_test.go
@@ -1,10 +1,35 @@
 package main
 
+import (
+	"sync"
+	"sync/atomic"
+	"testing"
+)
+
+// Counters interface -> better reuse later benchmark code
+type Counters interface {
+	RequestsPtr() *uint64
+	ErrorPtr() *uint64
+	LatencyPtr() *uint64
+}
+
 // CountersWithoutPadding will cause 'false-sharing'
 type CountersWithoutPadding struct {
 	requests uint64
 	errors   uint64
 	latency  uint64
+}
+
+func (c *CountersWithoutPadding) RequestsPtr() *uint64 {
+	return &c.requests
+}
+
+func (c *CountersWithoutPadding) ErrorPtr() *uint64 {
+	return &c.errors
+}
+
+func (c *CountersWithoutPadding) LatencyPtr() *uint64 {
+	return &c.latency
 }
 
 // CountersWithPadding since with extra padding, fill the whole cache line
@@ -18,8 +43,59 @@ type CountersWithPadding struct {
 	_        [128]byte
 }
 
-type Counters interface {
-	RequestsPtr() *uint64
-	ErrorPtr() *uint64
-	LatencyPtr() *uint64
+func (c *CountersWithPadding) RequestsPtr() *uint64 {
+	return &c.requests
+}
+
+func (c *CountersWithPadding) ErrorPtr() *uint64 {
+	return &c.errors
+}
+
+func (c *CountersWithPadding) LatencyPtr() *uint64 {
+	return &c.latency
+}
+
+// runBenchMark create multiple workers to do benchmark
+func runBenchMark(b *testing.B, counters Counters) {
+
+	var wg sync.WaitGroup
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		wg.Add(3)
+		go func() {
+			defer wg.Done()
+			ptr := counters.RequestsPtr()
+			for range 1000 {
+				atomic.AddUint64(ptr, 1)
+			}
+		}()
+		go func() {
+			defer wg.Done()
+			etr := counters.ErrorPtr()
+			for range 1000 {
+				atomic.AddUint64(etr, 1)
+			}
+		}()
+		go func() {
+			defer wg.Done()
+			ltr := counters.LatencyPtr()
+			for range 1000 {
+				atomic.AddUint64(ltr, 1)
+			}
+		}()
+
+		wg.Wait()
+	}
+}
+
+// BenchmarkHasFalseSharing 不使用padding, 可以看出最大也就是到 24176
+func BenchmarkHasFalseSharing(b *testing.B) {
+	var counters CountersWithoutPadding
+	runBenchMark(b, &counters)
+}
+
+func BenchmarkHasNoFalseSharing(b *testing.B) {
+	var counters CountersWithPadding
+	runBenchMark(b, &counters)
 }
