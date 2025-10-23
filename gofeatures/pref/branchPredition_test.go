508ec378922bcc50b1d0d4a074cccb41e118569d
Roylic <35546172+korisky@users.noreply.github.com>
2025-10-23 13:17:16 +0800

    fix: correct the cmt

--- gofeatures/pref/branchPredition_test.go
+++ gofeatures/pref/branchPredition_test.go
@@ -29,8 +29,8 @@ func CountConditionBranchless(data []int, threshold int) int {
 	count := 0
 	for _, v := range data {
 		// 1. 如果 v - threshold - 1 < 0, 我们主要看结果是正数还是负数
-		// 2. >> 63 获取最高位, 1正数, 0负数
-		// 3. 与1进行异或XOR操作, 1也就是正数也就添加,
+		// 2. >> 63 获取最高位, 1负数, 0正数
+		// 3. 与1进行异或XOR操作, 1也就是负数也就不添加, 0是正数则添加
 		count += int(uint(v-threshold-1)>>63) ^ 1
 	}
 	return count
