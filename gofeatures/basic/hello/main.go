4835d6eee7e71681244b306bdc035927886117a6
Roylic <35546172+korisky@users.noreply.github.com>
2025-10-25 14:38:56 +0800

    feat: update with the legacy logging settle

--- gofeatures/basic/hello/main.go
+++ gofeatures/basic/hello/main.go
@@ -2,10 +2,17 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"own/simple/greetings"
 )
 
 func main() {
-	msg := greetings.Hello("Gladys")
+	// predefined logger
+	log.SetPrefix("log-greetings: ")
+	log.SetFlags(log.LstdFlags | log.Lmicroseconds)
+	msg, err := greetings.Hello("")
+	if err != nil {
+		log.Fatal(err)
+	}
 	fmt.Print(msg)
 }
