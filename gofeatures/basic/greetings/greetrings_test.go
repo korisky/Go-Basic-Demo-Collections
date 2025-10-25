0439db9d10bd4f583ce976556f40856db056ca56
Roylic <35546172+korisky@users.noreply.github.com>
2025-10-25 23:37:30 +0800

    chore: add unit tests

---
+++ gofeatures/basic/greetings/greetrings_test.go
@@ -0,0 +1,22 @@
+package greetings
+
+import (
+	"regexp"
+	"testing"
+)
+
+func TestHelloName(t *testing.T) {
+	name := "Henry"
+	want := regexp.MustCompile(`\b` + name + `\b`)
+	msg, err := Hello(name)
+	if !want.MatchString(msg) || err != nil {
+		t.Errorf(`Hello("Henry") = %q, %v, want match for %#q, nil`, msg, err, want)
+	}
+}
+
+func TestHelloEmpty(t *testing.T) {
+	msg, err := Hello("")
+	if msg != "" || err == nil {
+		t.Errorf(`Hello("") = %q, %v, want "", error`, msg, err)
+	}
+}
