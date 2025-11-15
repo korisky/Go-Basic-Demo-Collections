e46020206d60dbf3ce427280ea175c2a3f1e1ac5
Roylic <35546172+korisky@users.noreply.github.com>
2025-11-15 11:43:21 +0800

    feat: adding flight-record init & integrate into server

--- webProjects/flightRecord/main.go
+++ webProjects/flightRecord/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"runtime/trace"
 	"sync"
 	"time"
 )
@@ -38,7 +39,25 @@ func handler() http.HandlerFunc {
 }
 
 // main is for simulation for checking the FlightRecord
+// Flight Record 使用类似时间轮的方式, 记录最近几次记录的tracing
+// 所以定义时间窗口的大小, 是使用的关键.
 func main() {
+
+	// flight-record config, 记住MAXBytes永远为上限, 一旦达到则直接记录即使没有达到配置的MinAge¬
+	fr_cfg := trace.FlightRecorderConfig{
+		MinAge:   5 * time.Second, // 定义fc的窗口lower-bound
+		MaxBytes: 3 << 20,         // 3MB, 1 << 10: 1kb, 1 << 20 Mb, 1 << 30: 1Gb
+	}
+
+	// start flight-record
+	fr := trace.NewFlightRecorder(fr_cfg)
+	err := fr.Start()
+	if err != nil {
+		log.Fatalf("unalbe to start trace flight recorder: %v", err)
+	}
+	defer fr.Stop()
+
+	// start serving
 	http.HandleFunc("/", handler())
 	log.Fatal(http.ListenAndServe("localhost:8080", nil))
 }
