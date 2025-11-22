c81d9b5344ded11d33734637c536f0ce420eb5e8
Roylic <35546172+korisky@users.noreply.github.com>
2025-11-22 13:57:02 +0800

    chore: adding combination

--- gofeatures/basic/go_fallback_val_test.go
+++ gofeatures/basic/go_fallback_val_test.go
@@ -3,14 +3,16 @@ package basic
 import (
 	"cmp"
 	"fmt"
+	"log"
 	"os"
+	"strings"
 	"testing"
 )
 
 // TestFallbackValCMPUsage 使用cmp进行环境参数获取&默认配置获取
 func TestFallbackValCMPUsage(t *testing.T) {
 	// cmp.Or() 逻辑是, 从前到后获取到第一个非nil的值, 则进行返回
-	port := cmp.Or(getPortFromEnv(), getPortFromFlag())
+	port := cmp.Or(getPortFromEnv(), getPortFromFlag(), "8080")
 	fmt.Println(port)
 }
 
@@ -20,4 +22,18 @@ func getPortFromFlag() string {
 
 func getPortFromEnv() string {
 	return os.Getenv("PORT")
+}
+
+func TestAnotherInputScanning(t *testing.T) {
+	name := cmp.Or(getUserNameFromInput(), "Anonymous")
+	fmt.Printf("Hello, %s\n", name)
+}
+
+func getUserNameFromInput() string {
+	fmt.Print("Enter your name, 'Anonymous' as default")
+	var name string
+	if _, err := fmt.Scanln(&name); err != nil {
+		log.Fatal(err)
+	}
+	return strings.TrimSpace(name)
 }
